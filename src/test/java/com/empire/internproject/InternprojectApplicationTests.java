23fa5e2af186e494eda48b0bf69ead2f3e7a89c5
Abhay <abhayborde8080@gmail.com>
2026-03-10 21:35:26 +0530

    update project

---
+++ src/test/java/com/empire/internproject/InternprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empire.internproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
